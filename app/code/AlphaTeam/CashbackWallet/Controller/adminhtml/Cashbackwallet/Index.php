a6725b3c65bd313e19b557c154e1e17c7b24a76d
Edson Galan <edd.galan@hotmail.com>
2024-12-29 16:16:24 -0600

    Create the view for 'cashback wallet index' - Create Cashbackwallet/Index Controller - Create adminhtml/routes.xml - Create layout Create the files needed for a basic view

---
+++ app/code/AlphaTeam/CashbackWallet/Controller/adminhtml/Cashbackwallet/Index.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace AlphaTeam\CashbackWallet\Controller\adminhtml\Cashbackwallet;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Controller\ResultFactory;
+
+/**
+ * Controller for the 'cashbackwallet_management/cashbackwallet/index' URL route.
+ */
+class Index extends Action implements HttpGetActionInterface
+{
+    /**
+     * Authorization level of a basic admin session.
+     */
+    public const ADMIN_RESOURCE = 'AlphaTeam_CashbackWallet::cashbackwallet_management';
+
+    /**
+     * Admin menu item
+     */
+    public const ADMIN_MENU = 'AlphaTeam_CashbackWallet::index';
+
+    /**
+     * Execute controller action.
+     *
+     * @return ResponseInterface|ResultInterface
+     */
+    public function execute(): ResultInterface|ResponseInterface
+    {
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->getConfig()->getTitle()->prepend(__('Cashback Wallet'));
+        $resultPage->setActiveMenu(static::ADMIN_MENU);
+
+        return $resultPage;
+    }
+}
