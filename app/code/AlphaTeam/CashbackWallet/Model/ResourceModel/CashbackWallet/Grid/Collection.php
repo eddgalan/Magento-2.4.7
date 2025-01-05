301205d662b57cc8a94f368c0ea3066cbfd02187
Edson Galan <edd.galan@hotmail.com>
2025-01-05 14:58:46 -0600

    Use Cashback Wallet\Grid\Collection to display the information in the grid

--- app/code/AlphaTeam/CashbackWallet/Model/ResourceModel/CashbackWallet/Grid/Collection.php
+++ app/code/AlphaTeam/CashbackWallet/Model/ResourceModel/CashbackWallet/Grid/Collection.php
@@ -6,4 +6,18 @@ use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
 
 class Collection extends SearchResult
 {
+    /**
+     * @return void
+     */
+    protected function _initSelect(): void
+    {
+        parent::_initSelect();
+        $this->getSelect()->joinLeft(
+            ['ce' => $this->getTable('customer_entity')],
+            'main_table.customer_id = ce.entity_id',
+            [
+                'customer_name' => new \Zend_Db_Expr("CONCAT(ce.firstname, ' ', ce.lastname)")
+            ]
+        );
+    }
 }
